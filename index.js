9658410feb932b84cfaf5dc2d25b3cd086554835
manisai66 <manikambalapalli@gmail.com>
2024-11-18 19:53:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+setTimeout(() => {
+    console.log("hi this mani");
+}, 5000);
+
+console.log("hello world!");
+setTimeout(() =>{
+    console.log("hi this is settimeout");
+}, 10000)
+
+setTimeout(() =>{
+    alert("oops wait mani");
+    console.log("this is 15seconds")
+},15000)
+
+// SETINTERVALS
+
+setInterval(() => {
+    console.log("this is interval")
+}, 3000);
+
+setInterval(() => {
+    console.log("1")
+}, 5000);
